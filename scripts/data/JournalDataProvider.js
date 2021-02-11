3214ce8c3178ed38827018cb2b3769c1e009085e
jon-xo <jonn@hey.com>
2021-02-11 15:21:53 -0500

    Moved data provider to entries.json. Updated functions to fetch from API. Created .gitignore file.

--- scripts/data/JournalDataProvider.js
+++ scripts/data/JournalDataProvider.js
@@ -9,37 +9,26 @@
  */
 
 // This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "01/05/2021",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Neutral"
-    },
-    {
-        id: 2,
-        date: "01/08/2021",
-        concept: "Agile",
-        entry: "Provided insight in first retro after completing team HTML & CSS chanllenge.",
-        mood: "Curious"
-    },
-    {
-        id: 3,
-        date: "01/11/2021",
-        concept: "JavaScript",
-        entry: "JavaScript function drills!",
-        mood: "Exhausted"
-    },
-]
+let journal = [];
 
 /*
     You export a function that provides a version of the
     raw data in the format that you want
 */
 
+export const useEntries = () => journal.slice();
+
+export const getEntries = () => {
+    return fetch("http://localhost:8088/journal")
+    .then(r => r.json())
+    .then(entriesReturned => {
+        journal = entriesReturned;
+    })
+};
+
 export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
+    const unsortedEntries = useEntries();
+    const sortedByDate = unsortedEntries.sort(
         (currentEntry, nextEntry) => {
             // console.log(currentEntry, nextEntry);
             // console.log(Date.parse(currentEntry.date), Date.parse(nextEntry.date));
--- scripts/data/JournalEntryList.js
+++ scripts/data/JournalEntryList.js
@@ -5,26 +5,30 @@
  *    data provider component
  */
 
-import { useJournalEntries } from "./JournalDataProvider.js";
+import { getEntries, useJournalEntries } from "./JournalDataProvider.js";
 import { JournalEntryComponent, JournalEntryHTML, JournalEntryAgile, JournalEntryJS } from "./JournalEntry.js";
 
 // Reference to DOM element for positioning
 const entryLog = document.querySelector('#entryLog');
 
 export const EntryListComponent = () => {
-    // Retreive a copy of the entry object from the data array.
-    const entries = useJournalEntries();
 
-    for (const entry of entries) {
-        // Call component that returns html of a single journal entry.
-        if (entry.concept === "HTML & CSS") {
-            entryLog.innerHTML += JournalEntryHTML(entry);
-        } else if (entry.concept === "Agile") {
-            entryLog.innerHTML += JournalEntryAgile(entry);
-        } else if (entry.concept === "JavaScript") {
-            entryLog.innerHTML += JournalEntryJS(entry);
-        } else {
-            entryLog.innerHTML += JournalEntryComponent(entry);
+    // Retreive a copy of the entry object from the data array.
+    
+    getEntries().then(() => {
+        const entries = useJournalEntries();
+        for (const entry of entries) {
+            // Call component that returns html of a single journal entry.
+            if (entry.concept === "HTML & CSS") {
+                entryLog.innerHTML += JournalEntryHTML(entry);
+            } else if (entry.concept === "Agile") {
+                entryLog.innerHTML += JournalEntryAgile(entry);
+            } else if (entry.concept === "JavaScript") {
+                entryLog.innerHTML += JournalEntryJS(entry);
+            } else {
+                entryLog.innerHTML += JournalEntryComponent(entry);
+            }
         }
-    }
+    })
+
 };
